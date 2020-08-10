6ddf22bda1f24ec85a8bb690e797c3dce9308dfc
Petori <zhoupei1111@yeah.net>
2020-08-10 17:42:40 +0800

    update dmpGrinding.cpp

--- ur_arm/src/dmpGrinding.cpp
+++ ur_arm/src/dmpGrinding.cpp
@@ -32,12 +32,17 @@ using namespace std;
 
 #define n_rfs 30 // 基函数个数
 sensor_msgs::JointState urState;  //机械臂当前状态
+string tool_frame = "tool0"; // 末端坐标系
 
 // dmp迭代函数
 Eigen::MatrixXd dmp_run_once(Eigen::MatrixXd previous_status, double goal, double tao, double dt, double wi[n_rfs]);
 void goPose(Eigen::MatrixXd now_status_x, Eigen::MatrixXd now_status_y, Eigen::MatrixXd now_status_z, ur_arm::PoseMatrix pose_);
 //获取机器人当前信息
 void getRobotInfo(sensor_msgs::JointState curState);
+//关节角控制方式，去起始位置
+void goStartPose();
+// 录轨迹数据用的测试位置
+void goTestPose();
 
 int main(int argc, char **argv)
 {
@@ -50,6 +55,17 @@ int main(int argc, char **argv)
 
   //ros::Publisher display_publisher = node_handle.advertise<moveit_msgs::DisplayTrajectory>("/move_group/display_planned_path", 1, true);
   ros::Subscriber joints_sub = node_handle.subscribe("/joint_states", 1, getRobotInfo);
+
+  //去初始位置
+  cout<<"去往初始姿态"<<endl;
+  goStartPose();
+  cout<<"到达初始姿态"<<endl;
+
+  // cout<<"是否开始执行测试轨迹？确定请按ENTER 不确定请Ctrl+C"<<endl;
+  // while(getchar()!='\n');
+  // cout<<"开始执行测试轨迹"<<endl;
+  // goTestPose();
+  // cout<<"执行完毕"<<endl;
 
   // 从文件读取圆心坐标
   ifstream fin("/home/petori/data/parameter/cc_pos.txt");
@@ -112,7 +128,24 @@ int main(int argc, char **argv)
   goal_z = goal_pos[2];
 
   double tao = 1;
-  double dt;
+  double dt = 0.004;
+
+  // 赋初值
+  previous_status_x(0,0) = 1;
+  previous_status_x(1,0) = startpos.p[0];
+  previous_status_x(2,0) = 0;
+  previous_status_x(3,0) = 0;
+
+  previous_status_y(0,0) = 1;
+  previous_status_y(1,0) = startpos.p[1];
+  previous_status_y(2,0) = 0;
+  previous_status_y(3,0) = 0;
+
+  previous_status_z(0,0) = 1;
+  previous_status_z(1,0) = startpos.p[2];
+  previous_status_z(2,0) = 0;
+  previous_status_z(3,0) = 0;
+
 
   while(now_status_x(0,0)>0)
   {
@@ -151,8 +184,9 @@ Eigen::MatrixXd dmp_run_once(Eigen::MatrixXd previous_status, double goal, doubl
 
   for(int i=0;i<n_rfs;i++)
   {
-    t_int[i] = i/(n_rfs-1);
+    t_int[i] = double(i)/(n_rfs-1);
     c[i] = exp(-1*alpha_x*t_int[i]);
+    //cout<<"c[i]:"<<c[i]<<endl;
     if(i>0)
     {
       h[i-1]=(c[i]-c[i-1])*0.55;
@@ -160,19 +194,25 @@ Eigen::MatrixXd dmp_run_once(Eigen::MatrixXd previous_status, double goal, doubl
   }
   h[n_rfs]=h[n_rfs-1];
 
+  //cout<<"previous_status: "<<previous_status(0,0)<<", "<<previous_status(1,0)<<", "<<previous_status(2,0)<<", "<<previous_status(3,0)<<endl;
   for(int i=0;i<n_rfs;i++)
   {
     psi(i,0) = exp(-0.5*(previous_status(0,0)-c[i])*(previous_status(0,0)-c[i])/(h[i]*h[i]));
     wii(i,0) = wi[i];
   }
 
+  // cout<<"Psi: "<<endl;
+  // cout<<psi<<endl;
+
   f = psi.transpose()*wii*previous_status(0,0)/psi.sum();
   f_value = f(0,0);
+  //cout<<"f_value: "<<f_value<<endl;
 
   now_status(3,0) = alpha_y*(beta_y*(goal-previous_status(1,0))-previous_status(2,0)) + f_value;
   now_status(2,0) = previous_status(2,0) + tao*now_status(3,0)*dt;
   now_status(1,0) = previous_status(1,0) + tao*now_status(2,0)*dt;
   now_status(0,0) = previous_status(0,0) - tao*alpha_x*previous_status(0,0)*dt;
+  //cout<<"now_status: "<<now_status(0,0)<<", "<<now_status(1,0)<<", "<<now_status(2,0)<<", "<<now_status(3,0)<<endl;
 
   return now_status;
 }
@@ -201,13 +241,56 @@ void goPose(Eigen::MatrixXd now_status_x, Eigen::MatrixXd now_status_y, Eigen::M
   goPose_.position.y = now_status_y(1,0);
   goPose_.position.z = now_status_z(1,0);
 
-  targetWayPoints.push_back(goPose_);
-  arm_group.computeCartesianPath(targetWayPoints,
-                                 0.02,  // eef_step
-                                 0.0,   // jump_threshold
-                                 trajectory);
-  move_plan.trajectory_ = trajectory;
+  cout<<"当前末端位置为: "<<pose_.p[0]<<", "<<pose_.p[1]<<", "<<pose_.p[2]<<endl;
+  cout<<"现在去往目标位置："<<goPose_.position.x<<", ";
+  cout<<goPose_.position.y<<", "<<goPose_.position.z<<endl;
 
-  arm_group.execute(move_plan);
+  arm_group.setPoseTarget(goPose_,tool_frame);
 
+  // targetWayPoints.push_back(goPose_);
+  // arm_group.computeCartesianPath(targetWayPoints,
+  //                                0.02,  // eef_step
+  //                                0.0,   // jump_threshold
+  //                                trajectory);
+  // move_plan.trajectory_ = trajectory;
+
+  arm_group.move();
+
+}
+
+void goStartPose()
+{
+  std::vector<double> sAng;
+  moveit::planning_interface::MoveGroupInterface arm_group("manipulator");
+
+  sAng.clear();
+  // 位置是[x y z] = [0.2616 -0.3663 0.1032]
+  sAng.push_back(1.95);
+  sAng.push_back(-1.31);
+  sAng.push_back(2.24);
+  sAng.push_back(-2.5);
+  sAng.push_back(-1.59);
+  sAng.push_back(-0.24);
+
+  arm_group.setJointValueTarget(sAng);
+  arm_group.move();
+}
+
+void goTestPose()
+{
+  std::vector<double> sAng2;
+  moveit::planning_interface::MoveGroupInterface arm_group("manipulator");
+
+  sAng2.clear();
+  // 位置是[x y z] = [0.3616 -0.0663 0.5032]
+  sAng2.push_back(2.7653);
+  sAng2.push_back(-1.6266);
+  sAng2.push_back(1.4359);
+  sAng2.push_back(-1.3692);
+  sAng2.push_back(-1.5834);
+  sAng2.push_back(0.5754);
+
+  arm_group.setJointValueTarget(sAng2);
+  arm_group.setMaxVelocityScalingFactor(0.05);// 慢点，方便录制
+  arm_group.move();
 }
